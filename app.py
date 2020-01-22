e2972969f690de85f6d37a9e824b5a4d0c1efb40
Taylor Grey <tagrey@dhcp-18-20-217-130.dyn.mit.edu>
2020-01-22 16:59:58 -0500

    Add file uploading. When file uploaded, print that it was successful. Output file contents when analyze is clicked

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+import os
 app = Flask(__name__)
+app.secret_key = 'wvjknsdvrbksvd609'
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+	return render_template('index.html')
 
-@app.route("/upload_document")
+@app.route("/upload_document", methods=['POST'])
 def upload_document():
-	return
+	file = request.files['file']
+	if file:
+		file.save("example.txt")
+		flash("Upload Successful!")
+		return render_template('index.html')
+
+@app.route("/analyze", methods=['GET'])
+def analyze():
+	with open('example.txt') as text_file:
+		flash(text_file.readlines())
+	return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
